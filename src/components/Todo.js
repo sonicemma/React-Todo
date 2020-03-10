cf9d55cfe5ea088f116ceefccd80f72506371a49
emma <sonicjane2020@gmail.com>
2020-03-09 23:13:16 -0700

    fixed functionality to cross out completed items

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,7 @@ const Todo = props => {
     return (
         <div
             onClick={() => props.toggleCompleted(props.item.id)}
-            className={`item${props.item.completed} ? ' completed' : ''}`}
+            className={`item${props.item.completed ? ' completed' : ''}`}
         >
             <p>{props.item.task}</p>
         </div>
